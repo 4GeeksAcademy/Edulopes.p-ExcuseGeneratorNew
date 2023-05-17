d0c8734c32dffae13062096890c443df4f5422cb
Edulopesp <129979638+Edulopesp@users.noreply.github.com>
2023-05-17 19:18:24 +0000

    ExcuseGenProject

--- src/app.js
+++ src/app.js
@@ -5,7 +5,41 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#phrase").innerHTML = generateExcuse();
+};
+let generateExcuse = () => {
+  let blamer = [
+    "My dog",
+    "My platypus",
+    "My hedgehog",
+    "My brother",
+    "My turtle",
+    "My shark"
+  ];
+  let action = ["ate", "destroyed", "putted fire on", "sold"];
+  let avoid = ["my passport", "my homework", "my 4geeks project", "my macbook"];
+  let when = [
+    "five minutes ago",
+    "10 minutes ago",
+    "yesterday",
+    "today",
+    "when I was born"
+  ];
+
+  let blamerIndex = Math.floor(Math.random() * blamer.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let avoidIndex = Math.floor(Math.random() * avoid.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    blamer[blamerIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    avoid[avoidIndex] +
+    " " +
+    when[whenIndex]
+  );
 };
